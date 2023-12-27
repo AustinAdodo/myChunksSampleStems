7b0672313ca5c85e69ca086a81d38cf4b2d534f2
Odia Adodo <subzbelow@gmail.com>
2023-12-27 18:19:13 +0100

    Update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,14 +10,14 @@ namespace WebApiExtension.Controllers
     public class HomeController : Controller
     {
         private const string uri = "https://raw.githubusercontent.com/qualified/challenge-data/master/words_alpha.txt";
-        public HomeController(){}
+        public HomeController() { }
 
         /// <summary>
         /// Produces a stem service result.
         /// </summary>
         /// <param name="stem"></param>
         /// <returns></returns>
-        
+
         [HttpGet]
         [ProducesResponseType(type: typeof(StemServiceResult), statusCode: StatusCodes.Status200OK)]
         [ProducesResponseType(statusCode: StatusCodes.Status404NotFound)]
@@ -43,7 +43,7 @@ namespace WebApiExtension.Controllers
 
                     if (query == "")
                     {
-                        StemServiceResult res = new StemServiceResult { Data = dataArray.ToList() }; 
+                        StemServiceResult res = new StemServiceResult { Data = dataArray.ToList() };
                         var json = JsonSerializer.Serialize(res); return new OkObjectResult(json);
                     }
 
@@ -53,7 +53,7 @@ namespace WebApiExtension.Controllers
                     }
 
                     List<string> filteredArray = dataArray.Where(item => item.StartsWith(query)).ToList();
-                    StemServiceResult result = new StemServiceResult {  Data = filteredArray};
+                    StemServiceResult result = new StemServiceResult { Data = filteredArray };
 
                     var json1 = JsonSerializer.Serialize(result);
                     return new OkObjectResult(json1);
@@ -64,6 +64,45 @@ namespace WebApiExtension.Controllers
                 }
             }
         }
+
+        public static async Task<string> BestIngenre(string country, string phoneNumber)
+        {
+            string uri = $"https://jonmock.hackerrank.com/api/countries?name={country}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(uri);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    // Deserialize the JSON response to dynamic object
+                    dynamic jsonData = Newtonsoft.Json.JsonConvert.DeserializeObject(responseBody);
+
+                    if (jsonData.data.Count == 0)
+                    {
+                        return "-1"; // Country not found
+                    }
+
+                    // Extracting calling codes
+                    string[] callingCodes = jsonData.data[0].callingcodes.ToObject<string[]>();
+
+                    // Get the highest index calling code
+                    string callingCode = callingCodes.Length > 0 ? callingCodes[callingCodes.Length - 1] : "";
+
+                    // Format the phone number
+                    string formattedPhoneNumber = $"+{callingCode} {phoneNumber}";
+
+                    return formattedPhoneNumber;
+                }
+                else
+                {
+                    return "-1"; // API request failed
+                }
+            }
+        }
+
     }
 }
 
